d9d52e67a48ec57a7c7a17324f11dd4c43c95edd
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-07-11 12:41:39 -0700

    Add files via upload

--- cs50p-learning-projects-full/Week4_Libraries/generate.py
+++ cs50p-learning-projects-full/Week4_Libraries/generate.py
@@ -1,8 +1,21 @@
 from random import choice 
+import random 
 
+
+'''
 coin = choice(["heads","tails"])
-
 #print(coin)
-
 for i in coin:
   print(coin)
+------------------------------------
+  
+
+number = random.randint(1,10)
+print(number)
+------------------------------------
+'''
+
+cards =["jack","queen","king"]
+random.shuffle(cards)
+for i in cards :
+  print(i)
