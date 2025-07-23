9cc1276fcb63210c9f274b458fd0592184ed5fbc
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-07-22 17:40:16 -0700

    Update itunes.py

--- cs50p-learning-projects-full/Week4_Libraries/itunes.py
+++ cs50p-learning-projects-full/Week4_Libraries/itunes.py
@@ -5,4 +5,8 @@ if len(sys.argv) != 2:
   sys.exit()
 
 respose = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+<<<<<<< Updated upstream
 print(respose.json())
+=======
+print(respose.json())
+>>>>>>> Stashed changes
