3f656b7a953ae1e788df29eedbc8aadfaa894a6a
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-07-22 17:12:49 -0700

    Update itunes.py

--- cs50p-learning-projects-full/Week4_Libraries/itunes.py
+++ cs50p-learning-projects-full/Week4_Libraries/itunes.py
@@ -4,4 +4,5 @@ import sys
 if len(sys.argv) != 2:
   sys.exit()
 
-requests.get("hhttps://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+respose = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+print(respose.json())
