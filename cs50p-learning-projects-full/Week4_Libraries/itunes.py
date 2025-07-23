56e9a7b48a5b3f4cf405cf2e67e27bfcaad55e2a
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-07-23 09:30:57 -0700

    Update itunes.py

--- cs50p-learning-projects-full/Week4_Libraries/itunes.py
+++ cs50p-learning-projects-full/Week4_Libraries/itunes.py
@@ -1,10 +1,21 @@
 import requests
 import sys
+import json
 
 if len(sys.argv) != 2:
   sys.exit()
 
 respose = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
-print(respose.json())
 
 
+
+o=respose.json()
+for i in o["results"]:
+  print(i["trackName"])
+
+
+
+"""
+print(json.dumps(respose.json(), indent=2))
+
+"""
