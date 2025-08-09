e966c52203bcea53dd9a3b0000ecfd1a271c0d2e
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2025-08-09 22:31:51 +0000

    update number.py

--- cs50p-learning-projects-full/Week3_Exceptions/number.py
+++ cs50p-learning-projects-full/Week3_Exceptions/number.py
@@ -1,3 +1,6 @@
-x=int(input("wahts x?"))
+try:
+  x=int(input("wahts x?"))
+  print(f"x is {x}")
+except ValueError:
+  print("check x you x is not allowd to be anything a part from a hole number")
 
-print(f"x is {x}")
